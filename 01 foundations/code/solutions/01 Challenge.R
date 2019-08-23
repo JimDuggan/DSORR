b6c4f0f37db73bfe2a4d6eb8e7da902dacd08d02
Jim Duggan <jim@MyMacBook.local>
2019-08-23 20:55:00 +0100

    Updating part 2

---
+++ 01 foundations/code/solutions/01 Challenge.R
@@ -0,0 +1,12 @@
+#  Challenge 1.1
+#  Determine the types for each of the following vectors
+v1 <- c(1L, T, FALSE)
+typeof(v1)
+v2 <- c(1L, T, FALSE, 2)
+typeof(v2)
+v3 <- c(T, FALSE, 2, "FALSE")
+typeof(v3)
+v4 <- c(2L, "FALSE")
+typeof(v4)
+v5 <- c(0L, 1L, 2.11)
+typeof(v5)
